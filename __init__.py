a1a9bddf80a412ef1cfaa2a1cd25bdf4be4985e4
Puneet Saini <puneet29saini@gmail.com>
2019-05-20 16:16:11 +0530

    Updated main and added 500

--- __init__.py
+++ __init__.py
@@ -7,9 +7,15 @@ app = Flask(__name__)
 def homepage():
     return render_template('main.html')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html')
 
 
 if __name__ == "__main__":

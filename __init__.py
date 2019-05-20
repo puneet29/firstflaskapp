2932057351ac262f53ef988ba93f0f5fe124113f
Puneet Saini <puneet29saini@gmail.com>
2019-05-20 17:26:38 +0530

    Added CMS using Python dict

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from content_management import Content
 
 app = Flask(__name__)
+TOP_DICT = Content()
 
 
 @app.route('/')
@@ -17,9 +19,11 @@ def page_not_found(e):
 def server_error(e):
     return render_template('500.html')
 
+
 @app.route('/dashboard/')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', TOP_DICT= TOP_DICT)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ content_management.py
@@ -0,0 +1,30 @@
+def Content():
+    TOP_DICT = {"spoj": [["ACODE", "https://github.com/puneet29/spoj/blob/master/ACODE.py"],
+                         ["ACPC10A", "https://github.com/puneet29/spoj/blob/master/ACPC10A.py"],
+                         ["ADDREV", "https://github.com/puneet29/spoj/blob/master/ADDREV.py"],
+                         ["AE00", "https://github.com/puneet29/spoj/blob/master/AE00.py"],
+                         ["AGGRCOW", "https://github.com/puneet29/spoj/blob/master/AGGRCOW.py"],
+                         ["BYTESM2", "https://github.com/puneet29/spoj/blob/master/BYTESM2.cpp"],
+                         ["CANDY", "https://github.com/puneet29/spoj/blob/master/CANDY.py"],
+                         ["COINS", "https://github.com/puneet29/spoj/blob/master/COINS.py"],
+                         ["DOUGHNUT", "https://github.com/puneet29/spoj/blob/master/DOUGHNUT.py"],
+                         ["FASHION", "https://github.com/puneet29/spoj/blob/master/FASHION.py"],
+                         ["FCTRL", "https://github.com/puneet29/spoj/blob/master/FCTRL.py"],
+                         ["FCTRL2", "https://github.com/puneet29/spoj/blob/master/FCTRL2.py"],
+                         ["NSTEPS", "https://github.com/puneet29/spoj/blob/master/NSTEPS.py"],
+                         ["ONP", "https://github.com/puneet29/spoj/blob/master/ONP.py"],
+                         ["PRIME1", "https://github.com/puneet29/spoj/blob/master/PRIME1.py"],
+                         ["SAMER08F", "https://github.com/puneet29/spoj/blob/master/SAMER08F.cpp"],
+                         ["TEST", "https://github.com/puneet29/spoj/blob/master/TEST.py"],
+                         ["TOANDFRO", "https://github.com/puneet29/spoj/blob/master/TOANDFRO.py"]
+                         ],
+                "notebooks": [["Image Style Transfer", "https://github.com/puneet29/notebooks/blob/master/Image_Style_Transfer.ipynb"],
+                              ["Udacity Pytorch Scholarship",
+                                  "https://github.com/puneet29/notebooks/blob/master/Udacity_Pytorch_Scholarship"]
+                              ],
+                "udacityproj": [["Demo Videos", "https://github.com/puneet29/Udacity-Projects/tree/master/Demo%20Videos"],
+                                ["Quiz", "https://github.com/puneet29/Udacity-Projects/tree/master/Quiz"],
+                                ["Tic Tac Toe", "https://github.com/puneet29/Udacity-Projects/tree/master/TicTacToe"]
+                                ]
+                }
+    return(TOP_DICT)

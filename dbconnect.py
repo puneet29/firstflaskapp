3554f61e4b10c145cf808db3241df3981ad5d713
Puneet Saini <puneet29saini@gmail.com>
2019-05-21 15:50:38 +0530

    Modified dbconnect

--- dbconnect.py
+++ dbconnect.py
@@ -1,6 +1,7 @@
 import mysql.connector as mysqldb
 
 def connection():
+    #Add your password below
     conn = mysqldb.connect(user='root', password='password', host='localhost', database='flask_users')
     c = conn.cursor()
     return(c, conn)
